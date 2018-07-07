6dde199edeb367d16135a4e2d0f006544fac2891
Ben <zhougy0717@gmail.com>
2018-07-07 18:54:04 +0800

    Rearch PopupWindowHandler.

--- app/src/main/java/cn/zhougy0717/xposed/niwatori/PopupWindowHandler.java
+++ app/src/main/java/cn/zhougy0717/xposed/niwatori/PopupWindowHandler.java
@@ -1,18 +1,17 @@
 package cn.zhougy0717.xposed.niwatori;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.PopupWindow;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -24,32 +23,61 @@ import de.robv.android.xposed.XposedHelpers;
  */
 
 public class PopupWindowHandler extends XposedModule{
-    private static final String FIELD_FLYING_HELPER = NFW.NAME + "_flyingHelper";
+    private static final String FIELD_FLYING_HANDLER = NFW.NAME + "_flyingHandler";
 
-    private static Handler mActiveHandler = null;
+    private static FlyingHandler mActiveHandler = null;
     private static Activity mCurrentActivity = null;
     private final static String FIELD_ACTIVITY_HANDLER = "activity_handler";
 
-    private class Handler{
-        private PopupWindow mPopupWindow;
+    private static class FlyingHandler {
+        private PopupWindow mPopupWindow = null;
         private FrameLayout mDecorView;
         private IReceiver mActionReceiver;
         private FlyingHelper mHelper;
         private IReceiver mSettingsLoadedReceiver;
-        public Handler(PopupWindow pw){
-            mPopupWindow = pw;
-            mDecorView = (FrameLayout) XposedHelpers.getObjectField(pw, "mDecorView");
+        private GestureDetector mEdgeGesture;
+        public static FlyingHandler create(FrameLayout decorView) {
+            FlyingHandler handler = (FlyingHandler) XposedHelpers.getAdditionalInstanceField(decorView, FIELD_FLYING_HANDLER);
+            if (handler == null) {
+                handler = new FlyingHandler(decorView);
+                XposedHelpers.setAdditionalInstanceField(decorView, FIELD_FLYING_HANDLER, handler);
+            }
+            return handler;
+        }
+        public static FlyingHandler create(PopupWindow pw) {
+            FrameLayout decorView = (FrameLayout) XposedHelpers.getObjectField(pw, "mDecorView");
+            return create(decorView);
+        }
 
+        private FlyingHandler(FrameLayout decorView){
+            mDecorView = decorView;
             mHelper = ModActivity.createFlyingHelper(mDecorView);
             mActionReceiver = ActionReceiver.getInstance(mDecorView, NFW.FOCUSED_DIALOG_FILTER);
             mSettingsLoadedReceiver = SettingsLoadReceiver.getInstance(mDecorView, NFW.SETTINGS_CHANGED_FILTER);
+
+            mEdgeGesture = new GestureDetector(mDecorView.getContext(), new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onFling(MotionEvent event1, MotionEvent event2,float velocityX, float velocityY) {
+                    if (!mHelper.edgeDetected(event1)) {
+                        return false;
+                    }
+                    NFW.performAction(mDecorView.getContext(), NFW.ACTION_SMALL_SCREEN);
+                    return true;
+                }
+
+                @Override
+                public boolean onDown(MotionEvent event){
+                    if (!edgeDetected(event)) {
+                        return false;
+                    }
+                    return true;
+                }
+            });
         }
 
         public void registerReceiver(){
             mActionReceiver.register();
             mSettingsLoadedReceiver.register();
-            View v = (View) XposedHelpers.getObjectField(mPopupWindow, "mContentView");
-//            mHelper.setForeground(v);
         }
 
         public void unregisterReceiver(){
@@ -87,6 +115,17 @@ public class PopupWindowHandler extends XposedModule{
                 }
             }
         }
+
+        public boolean edgeDetected(MotionEvent event){
+            return mHelper.edgeDetected(event);
+        }
+
+        public boolean onTouchEvent(MotionEvent event){
+            if(mEdgeGesture.onTouchEvent(event)) {
+                return true;
+            }
+            return mHelper.onTouchEvent(event);
+        }
     }
 
     public void install() {
@@ -115,25 +154,9 @@ public class PopupWindowHandler extends XposedModule{
                             return;
                         }
                         try {
-                            mActiveHandler = new PopupWindowHandler.Handler(pw);
+                            mActiveHandler = FlyingHandler.create(pw);
                             mActiveHandler.registerReceiver();
-                            XposedHelpers.setAdditionalInstanceField(mCurrentActivity, FIELD_ACTIVITY_HANDLER, mActiveHandler);
                             mActiveHandler.dealWithPersistentIn();
-                            final PopupWindow.OnDismissListener dismissListener =
-                                    (PopupWindow.OnDismissListener) XposedHelpers.getObjectField(pw, "mOnDismissListener");
-                            pw.setOnDismissListener(new PopupWindow.OnDismissListener() {
-                                @Override
-                                public void onDismiss() {
-                                    if (mActiveHandler != null) {
-                                        mActiveHandler.dealWithPersistentOut();
-                                        mActiveHandler.unregisterReceiver();
-                                        mActiveHandler = null;
-                                    }
-                                    if (dismissListener != null) {
-                                        dismissListener.onDismiss();
-                                    }
-                                }
-                            });
                         } catch (Throwable t) {
                             logE(t);
                         }
@@ -142,39 +165,36 @@ public class PopupWindowHandler extends XposedModule{
             }
         });
 
+        XposedBridge.hookAllMethods(PopupWindow.class, "dismiss", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                if (mActiveHandler != null) {
+                    mActiveHandler.dealWithPersistentOut();
+                    mActiveHandler.unregisterReceiver();
+                    mActiveHandler = null;
+                }
+            }
+        });
+
         final Class<?> classPopupDecorView = XposedHelpers.findClass("android.widget.PopupWindow$PopupDecorView", null);
-        XposedHelpers.findAndHookMethod(classPopupDecorView, "onTouchEvent", MotionEvent.class,
+        XposedHelpers.findAndHookMethod(classPopupDecorView, "dispatchTouchEvent", MotionEvent.class,
                 new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
                         try {
                             final FrameLayout decorView = (FrameLayout) methodHookParam.thisObject;
-                                final MotionEvent event = (MotionEvent) methodHookParam.args[0];
-//                                final FlyingHelper helper = getHelper((FrameLayout) decorView);
-                                final FlyingHelper helper = ModActivity.getHelper(decorView);
-                                if (helper != null && helper.onTouchEvent(event)) {
-                                    return true;
-                                }
+                            final MotionEvent event = (MotionEvent) methodHookParam.args[0];
+                            FlyingHandler handler = FlyingHandler.create(decorView);
+                            if (handler.onTouchEvent(event)) {
+                                return true;
+                            }
                         } catch (Throwable t) {
                             logE(t);
                         }
                         return invokeOriginalMethod(methodHookParam);
                     }
                 });
-
-        XposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                // TODO: clear the existing handler under this activity's name
-                Activity activity = (Activity) param.thisObject;
-            }
-        });
     }
-
-//    private static FlyingHelper getHelper(@NonNull FrameLayout decorView) {
-//        return (FlyingHelper) XposedHelpers.getAdditionalInstanceField(
-//                decorView, FIELD_FLYING_HELPER);
-//    }
 
     public static void onPause(Activity activity){
         // TODO: save the handler under the activity's name
@@ -187,10 +207,9 @@ public class PopupWindowHandler extends XposedModule{
     public static void onResume(Activity activity){
         // TODO: check the existing handler using the activity's class name
         mCurrentActivity = activity;
-        Handler handler= (Handler) XposedHelpers.getAdditionalInstanceField(mCurrentActivity, FIELD_ACTIVITY_HANDLER);
-        if (handler != null){
-            handler.registerReceiver();
-            handler.dealWithPersistentIn();
+        if(mActiveHandler != null){
+            mActiveHandler.registerReceiver();
+            mActiveHandler.dealWithPersistentIn();
         }
     }
 }
--- app/src/main/java/cn/zhougy0717/xposed/niwatori/ReceiverManager.java
+++ app/src/main/java/cn/zhougy0717/xposed/niwatori/ReceiverManager.java
@@ -9,21 +9,11 @@ public class ReceiverManager {
     private FrameLayout mDecorView;
     private IReceiver mActionReceiver;
     private IReceiver mSettingsLoadedReceiver;
-//    private FlyingHelper mHelper;
     public ReceiverManager(FrameLayout decorView) {
         mDecorView = decorView;
         mActionReceiver = ActionReceiver.getInstance(mDecorView, NFW.FOCUSED_ACTIVITY_FILTER);
         mSettingsLoadedReceiver = SettingsLoadReceiver.getInstance(mDecorView, NFW.SETTINGS_CHANGED_FILTER);
     }
-
-//        private static Handler getInstance(FrameLayout decorView){
-//            Handler handler = (Handler) XposedHelpers.getAdditionalInstanceField(decorView, "HANDLER");
-//            if (handler == null) {
-//                handler = new Handler(decorView);
-//                XposedHelpers.setAdditionalInstanceField(decorView, "HANDLER", handler);
-//            }
-//            return handler;
-//        }
 
     public void registerReceiver(){
         mActionReceiver.register();
